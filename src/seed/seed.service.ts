3c42e6c179e6788083957956f695c05b94977b56
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-08-07 16:52:01 -0500

    adaptador

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
+
 import { PokeRespose } from './interface/poke-respose.interface';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
 import { Pokemon } from '../pokemon/entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
   constructor(
     @InjectModel(Pokemon.name) private readonly PokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
   async executeSeed() {
     await this.PokemonModel.deleteMany({});
-    const { data } = await this.axios.get<PokeRespose>(
+    const data = await this.http.get<PokeRespose>(
       'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
     const pokemonToInsert: { name: string; no: number }[] = [];
